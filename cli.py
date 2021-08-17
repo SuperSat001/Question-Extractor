4397571e53d9973530f48d827ffc16e04952dd5a
SuperSat001 <satyankar.lms@gmail.com>
2021-08-17 15:24:04 +0530

    initial cli

---
+++ cli.py
@@ -0,0 +1,60 @@
+from PIL import Image as img
+import matplotlib.pyplot as plt
+from functions import work
+
+def imgMerge(images):
+	widths, heights = zip(*(i.size for i in images))
+
+	total_height = sum(heights)
+	max_width = max(widths)
+
+	new_im = img.new('RGB', (max_width, total_height))
+
+	y_offset = 0
+	for im in images:
+		new_im.paste(im, (0, y_offset))
+		y_offset += im.size[1]
+
+	return new_im
+
+
+def page(im, x=1):
+	qns = work(im)
+	i = x
+	merge = False
+	to_merge = []
+	for qn in qns:
+		plt.imshow(qn)
+		plt.show()
+
+		if merge:
+			print("merging", len(to_merge))
+		t = int(input("Command? "))
+		if t == 0:
+			if merge:
+				new = imgMerge(to_merge)
+				new.save(f"cut/s {i}.png", "PNG")
+				to_merge = []
+				merge = False
+				i += 1
+		elif t == 1:
+			if not merge:
+				qn.save(f"cut/s {i}.png", "PNG")
+			else:
+				new = imgMerge(to_merge)
+				new.save(f"cut/s {i}.png", "PNG")
+				to_merge = []
+				merge = False
+			i += 1
+		elif t == 2:
+			if not merge:
+				merge = True
+				print(f"Merge started at q{i}")
+			to_merge.append(qn)
+
+
+
+sample = img.open(f"sample.png", "r")
+sample2 = img.open(f"sample2.png", "r")
+
+page(sample2)
--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 from PIL import Image as img
-from numpy import array
-from functions import *
+import matplotlib.pyplot as plt
+from functions import work
 
 sample = img.open(f"sample.png", "r")
 w, h = sample.size
@@ -9,15 +9,14 @@ print(w, h)
 
 pix = sample.load()
 
-print(notAllWhite(sample))
-print(checkWhite(100,100,pix))
-print(checkLine(sample))
-
 k = work(sample)
 print(k)
 
-k[0].save("ceh.png", "PNG")
+plt.imshow(k[0])
+plt.show()
 
+plt.imshow(k[1])
+plt.show()
 
 
 
